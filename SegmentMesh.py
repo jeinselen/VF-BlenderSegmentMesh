71b3b0d52f12a31d37e18b1cd01b0b2737d35c95
John Einselen VF <jeinselen@vectorform.com>
2023-11-09 19:37:10 -0500

    Increase settings granularity and fixed zero-selection error

--- SegmentMesh.py
+++ SegmentMesh.py
@@ -4,11 +4,15 @@
 import bpy
 from mathutils import Vector
 
-object_name = "Buildings"
-startX = -100.0
-startY = -100.0
-size = 20.0
-tiles = 10
+#object_name = "Buildings"
+#object_name = "Roads"
+object_name = bpy.context.active_object.name
+startX = -625.0
+startY = -500.0
+sizeX = 250.0
+sizeY = 250.0
+tileX = 5
+tileY = 4
 
 # Get the mesh object by its name
 mesh_object = bpy.data.objects[object_name]
@@ -21,12 +25,14 @@ print("Active 0: " + bpy.context.view_layer.objects.active.name)
 min_x = 0.0
 min_y = 0.0
 
-for x in range(tiles):
-    min_x = startX + (x * size)
-    max_x = min_x + size
-    for y in range(tiles):
-        min_y = startY + (y * size)
-        max_y = min_y + size
+for x in range(tileX):
+    min_x = startX + (x * sizeX)
+    max_x = min_x + sizeX
+    for y in range(tileY):
+        min_y = startY + (y * sizeY)
+        max_y = min_y + sizeY
+        
+        print("")
         
         print("Min: " + str(min_x) + " x " + str(min_y))
         print("Max: " + str(max_x) + " x " + str(max_y))
@@ -37,6 +43,9 @@ for x in range(tiles):
         # Create tile name
         tile_name = mesh_object.name + "-Tile-" + str(x) + "-" + str(y)
         print(tile_name)
+        
+        # Count how many polygons have been selected
+        count = 0
         
         # Select polygons within the specified XYZ area
         for polygon in mesh.polygons:
@@ -49,29 +58,37 @@ for x in range(tiles):
             # Check polygon centre against min/max
             if min_x <= centroid.x <= max_x and min_y <= centroid.y <= max_y:
                 polygon.select = True
+                count += 1
             else:
                 polygon.select = False
         
-        print("Active 2: " + mesh_object.name)
-        print("Active 2: " + bpy.context.view_layer.objects.active.name)
+        print("Selected: " + str(count))
         
-        # Separate selected polygons into a new object
-        bpy.context.view_layer.objects.active = mesh_object
-        print("Active 3: " + mesh_object.name)
-        print("Active 3: " + bpy.context.view_layer.objects.active.name)
-        mesh_object.select_set(True)
-        bpy.ops.object.mode_set(mode='EDIT')
-        bpy.ops.mesh.separate(type='SELECTED')
-        bpy.ops.object.mode_set(mode='OBJECT')
-        print("Active 4: " + mesh_object.name)
-        print("Active 4: " + bpy.context.view_layer.objects.active.name)
-        
-        # Rename the separated object and mesh
-        separated_object = bpy.context.selected_objects[1]
-        separated_object.name = tile_name
-        separated_mesh = separated_object.data
-        separated_mesh.name = tile_name
-        separated_object.select_set(False)
-        
-        print("Active 5: " + mesh_object.name)
-        print("Active 5: " + bpy.context.view_layer.objects.active.name)
+        # Only create a new segment if there are 1 or more polygons selected
+        if count > 0:
+            
+            print("Active 2: " + mesh_object.name)
+            print("Active 2: " + bpy.context.view_layer.objects.active.name)
+            
+            # Separate selected polygons into a new object
+            bpy.context.view_layer.objects.active = mesh_object
+            print("Active 3: " + mesh_object.name)
+            print("Active 3: " + bpy.context.view_layer.objects.active.name)
+            mesh_object.select_set(True)
+            bpy.ops.object.mode_set(mode='EDIT')
+            bpy.ops.mesh.separate(type='SELECTED')
+            bpy.ops.object.mode_set(mode='OBJECT')
+            print("Active 4: " + mesh_object.name)
+            print("Active 4: " + bpy.context.view_layer.objects.active.name)
+            
+            # Rename the separated object and mesh
+            separated_object = bpy.context.selected_objects[1]
+            separated_object.name = tile_name
+            separated_mesh = separated_object.data
+            separated_mesh.name = tile_name
+            separated_object.select_set(False)
+            
+            print("Active 5: " + mesh_object.name)
+            print("Active 5: " + bpy.context.view_layer.objects.active.name)
+            
+            print("")
